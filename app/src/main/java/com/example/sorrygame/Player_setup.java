98216d329b6eceba5b2845ce7cc5da7a5e2e8fc9
Luke Michel <lmichel5@ivytech.edu>
2021-04-06 15:54:48 -0400

    added title screen

---
+++ app/src/main/java/com/example/sorrygame/Player_setup.java
@@ -0,0 +1,14 @@
+package com.example.sorrygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Player_setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_setup);
+    }
+}
